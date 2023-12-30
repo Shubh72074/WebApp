faa4fc1eed59e4b252681b6a575138e50b08c670
Shubh72074 <Shubhamnishadr16@gmail.com>
2023-12-30 23:53:52 +0530

    added two endpoints and parsing url encoded data

--- src/main/java/com/shubh/WebApp/RestControllers.java
+++ src/main/java/com/shubh/WebApp/RestControllers.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class RestControllers {
     @GetMapping("/")
@@ -14,5 +17,13 @@ public class RestControllers {
     @GetMapping("/name/{n}")
     public String Name(@PathVariable String n) {
         return "Name: " + n;
+    }
+
+    @GetMapping("/all")
+    public List Details() {
+        List<Users> UsersT = new ArrayList<>();
+        UsersT.add(new Users(1,"Shubham Nishad"));
+        UsersT.add(new Users(2,"Saurav Nishad"));
+        return UsersT;
     }
 }
